1e0a60355cbfb7db8748fb9ff658e0366339ce46
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2025-07-09 22:46:02 -0300

    fix: base path in html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const basePath = process.env.BASE_PATH || ''
+  
   return (
     <Html lang="en">
       <Head>
@@ -8,14 +10,14 @@ export default function Document() {
         <meta name="description" content="" />
         <meta name="author" content="" />
         {/* Favicon */}
-        <link rel="icon" type="image/x-icon" href="/assets/img/salad.png" />
+        <link rel="icon" type="image/x-icon" href={`${basePath}/assets/img/salad.png`} />
         {/* Font Awesome icons (free version) */}
         <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossOrigin="anonymous"></script>
         {/* Google fonts */}
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         {/* Core theme CSS (includes Bootstrap) */}
-        <link href="/css/styles.css" rel="stylesheet" />
+        <link href={`${basePath}/css/styles.css`} rel="stylesheet" />
       </Head>
       <body id="page-top" suppressHydrationWarning>
         <Main />
@@ -26,10 +28,10 @@ export default function Document() {
         {/* Third party plugin JS */}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
         {/* Contact form JS */}
-        <script src="/assets/mail/jqBootstrapValidation.js"></script>
-        <script src="/assets/mail/contact_me.js"></script>
+        <script src={`${basePath}/assets/mail/jqBootstrapValidation.js`}></script>
+        <script src={`${basePath}/assets/mail/contact_me.js`}></script>
         {/* Core theme JS */}
-        <script src="/js/scripts.js"></script>
+        <script src={`${basePath}/js/scripts.js`}></script>
       </body>
     </Html>
   )
